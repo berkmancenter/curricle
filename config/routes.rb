e5b188a14d43d6dd170e4724fd2980099e344e96
Ryan Clardy <ryan@metabahn.com>
2016-12-15 13:31:46 -0600

    Refactor search controller to courses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'path', to: 'path#index'
-  get 'search', to: 'search#index'
-  root 'search#index'
+  get 'courses', to: 'courses#index'
+  root 'courses#index'
 end
