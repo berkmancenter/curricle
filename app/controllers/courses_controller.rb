a1707fb12e9cdd8bd49907910c654f92d1e3051c
Ryan Clardy <ryan@metabahn.com>
2016-12-20 17:47:29 -0600

    Hookup add/remove to try
    This commit also includes a change that stores the search keywords in
    the session and moves the bulk of the data crunching to courses#index.
    This allows your most recent search to persist across reloads.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,31 +4,52 @@ class CoursesController < ApplicationController
   def index
     @matching_courses ||= []
     @course_groups ||= []
+
+    if @keywords = session[:keywords]
+      query = Course.search_for(@keywords)
+
+      @course_groups = []
+
+      Course.subjects(query).each do |subject|
+        group = {
+          subject: subject,
+          days: [
+            { name: 'Mon', courses: Course.for_day(:monday, subject_academic_org_description: subject) },
+            { name: 'Tue', courses: Course.for_day(:tuesday, subject_academic_org_description: subject) },
+            { name: 'Wed', courses: Course.for_day(:wednesday, subject_academic_org_description: subject) },
+            { name: 'Thu', courses: Course.for_day(:thursday, subject_academic_org_description: subject) },
+            { name: 'Fri', courses: Course.for_day(:friday, subject_academic_org_description: subject) },
+          ]
+        }
+
+        not_empty = group[:days].find { |day| !day[:courses].empty? }
+        @course_groups << group if not_empty
+      end
+
+      @matching_courses = query.all
+    end
   end
 
   def search
-    query = Course.search_for(params["query"])
+    session[:keywords] = params["keywords"]
+    redirect_to '/'
+  end
 
-    @course_groups = []
+  def add_to_tray
+    course = Course.find(params["id"])
 
-    Course.subjects(query).each do |subject|
-      group = {
-        subject: subject,
-        days: [
-          { name: 'Mon', courses: Course.for_day(:monday, subject_academic_org_description: subject) },
-          { name: 'Tue', courses: Course.for_day(:tuesday, subject_academic_org_description: subject) },
-          { name: 'Wed', courses: Course.for_day(:wednesday, subject_academic_org_description: subject) },
-          { name: 'Thu', courses: Course.for_day(:thursday, subject_academic_org_description: subject) },
-          { name: 'Fri', courses: Course.for_day(:friday, subject_academic_org_description: subject) },
-        ]
-      }
+    UserCourse.find_or_create_by(user_id: current_user.id, course_id: course.id)
 
-      not_empty = group[:days].find { |day| !day[:courses].empty? }
-      @course_groups << group if not_empty
+    redirect_to '/'
+  end
+
+  def remove_from_tray
+    course = Course.find(params["id"])
+
+    if user_course = UserCourse.find_by(user_id: current_user.id, course_id: course.id)
+      user_course.destroy
     end
 
-    @matching_courses = query.all
-
-    render :index
+    redirect_to '/'
   end
 end
