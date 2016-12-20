92fdc23c8759669923addbc83ccf5ff94f2b9a66
Ryan Clardy <ryan@metabahn.com>
2016-12-20 07:48:11 -0600

    Basic search

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,5 +2,12 @@ class CoursesController < ApplicationController
   before_action :require_auth
   
   def index
+    @matching_courses ||= []
+  end
+
+  def search
+    @matching_courses = Course.search_for(params["query"])
+    @no_matches = @matching_courses.empty?
+    render :index
   end
 end
