8c78a463ddc5cb04a36540800deffa4d4ef2ef95
Ryan Clardy <ryan@metabahn.com>
2017-01-12 11:49:40 -0600

    Hookup backend for path semester view

--- app/controllers/path_controller.rb
+++ app/controllers/path_controller.rb
@@ -2,5 +2,55 @@ class PathController < ApplicationController
   before_action :require_auth
 
   def index
+    # TODO: scope this to only courses for the selected semester
+    @meeting_patterns = current_user.patterns_for_all_courses
+    @meeting_patterns_per_day = {
+      monday: current_user.patterns_for_all_courses(:monday),
+      tuesday: current_user.patterns_for_all_courses(:tuesday),
+      wednesday: current_user.patterns_for_all_courses(:wednesday),
+      thursday: current_user.patterns_for_all_courses(:thursday),
+      friday: current_user.patterns_for_all_courses(:friday)
+    }
+  end
+
+  # add course meeting pattern to current user's path
+  def add
+    @pattern = CourseMeetingPattern.find(params[:pattern_id])
+
+    user_course = UserCourse.find_or_initialize_by(
+      user_id: current_user.id,
+      course_id: @pattern.course_id,
+      course_meeting_pattern_id: [nil, @pattern.id]
+    )
+
+    user_course.course_meeting_pattern_id = @pattern.id
+    user_course.include_in_path = true
+    user_course.save
+
+    respond_to do |format|
+      format.html { redirect_to '/path' }
+      format.js
+    end
+  end
+
+  # remove course meeting pattern from current user's path
+  def remove
+    @pattern = CourseMeetingPattern.find(params[:pattern_id])
+
+    user_course = UserCourse.find_or_initialize_by(
+      user_id: current_user.id,
+      course_id: @pattern.course_id,
+    )
+
+    if user_course = UserCourse.find_by(user_id: current_user.id, course_meeting_pattern_id: @pattern.id)
+      user_course.course_meeting_pattern_id = nil
+      user_course.include_in_path = false
+      user_course.save
+    end
+
+    respond_to do |format|
+      format.html { redirect_to '/path' }
+      format.js
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,22 @@ class User < ApplicationRecord
 
   has_many :user_courses
   has_many :courses, through: :user_courses
+  has_many :path_meeting_patterns, through: :user_courses, source: :course_meeting_pattern
 
   def added?(course_id)
     courses.find_by(id: course_id)
+  end
+
+  def in_path?(pattern_id)
+    user_courses.find_by(
+      include_in_path: true,
+      course_meeting_pattern_id: pattern_id
+    )
+  end
+
+  def patterns_for_all_courses(by_day = nil)
+    query = CourseMeetingPattern.where(course: courses)
+    query = query.where("meets_on_#{by_day}": true) if by_day.present?
+    query
   end
 end
