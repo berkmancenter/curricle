8f2e27b707ede81980ec5201d40c7d29394592a6
Ryan Clardy <ryan@metabahn.com>
2017-01-13 10:30:38 -0600

    Hookup backend for 4-year view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,36 @@ module ApplicationHelper
 
     text
   end
+
+  # returns the current semester (e.g. "Spring 2017")
+  def current_semester
+    year = Time.now.year
+    name = Time.now.month < 6 ? "Spring" : "Fall"
+    "#{name} #{year}"
+  end
+
+  def semester_map(starts_at = nil)
+    starts_at ||= current_semester
+    name, year = starts_at.split(" ")
+    year = year.to_i
+
+    if name == "Spring"
+      # if current/provided semester is Spring, start with the fall of the previous year
+      year = year - 1
+    end
+
+    years = []
+
+    4.times do
+      current_year = []
+
+      current_year << "Fall #{year}"
+
+      year = year + 1
+      current_year << "Spring #{year}"
+      years << current_year
+    end
+
+    years
+  end
 end
