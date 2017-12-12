f4a7b78b3eb9a9b85c37425e1a8b05c402ccf56b
Patrick Lewis <patrick@endpoint.com>
2017-12-12 15:06:38 +0000

    Update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206153524) do
+ActiveRecord::Schema.define(version: 20170327173904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
