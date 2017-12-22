d8d6e867ce68f0749a6ef4439ba51162e18c2cbc
Patrick Lewis <patrick@endpoint.com>
2017-12-22 16:06:39 +0000

    Clean up routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'landing#index'
 
-  get '/courses/search', to: 'courses#search'
   resources :courses do
     collection do
       get :categories
+      get :fullsearch
+      get :search
     end
   end
-  get '/courses/fullsearch', to: 'courses#fullsearch'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
