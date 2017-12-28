6208945fe3792cd11897b9c82b5e53585ad9de59
Selvakumar Arumugam <selva@endpoint.com>
2017-12-28 05:56:41 +0000

    Add missing fields to course seed data and improve seeding - 15034

--- db/seeds/course_seeds.rb
+++ db/seeds/course_seeds.rb
@@ -2,30 +2,32 @@ require 'smarter_csv'
 
 puts "Seeding Courses!"
 
-courses = SmarterCSV.process(Rails.root.join('lib', 'seeds', 'courses.csv'))
-puts "csv object : #{courses.inspect}"
-courses.each do |row|
-  term_year, term_name = row[:term_description].to_s.split(' ')
-  c = Course.find_or_create_by(
-    external_course_id: row[:course_id],
-    course_description: row[:course_descr],
-    title: row[:course_title_long],
-    term_name: term_name,
-    term_year: term_year,
-    academic_year: row[:academic_year],
-    component: row[:component_description],
-    subject: row[:subject],
-    subject_description: row[:subject_description],
-    subject_academic_org_description: row[:subject_acad_org_description],
-    academic_group: row[:acad_group],
-    academic_group_description: row[:acad_group_description],
-    grading_basis_description: row[:grading_gasis_description],
-    term_pattern_code: row[:term_pattern_code],
-    term_pattern_description: row[:term_pattern_description],
-    units_maximum: row[:units_maximum],
-    catalog_number: row[:catalog_number],
-    course_description_long: row[:course_descrlong],
-    course_note: row[:course_note]
-  )
-  puts "'#{c.course_description}' saved"
+courses = SmarterCSV.process(Rails.root.join('lib', 'seeds', 'courses.csv'), chunk_size: 1000)  do |chunk|
+  chunk.each do |row|
+    term_year, term_name = row[:term_description].to_s.split(' ')
+    c = Course.find_or_create_by(
+      external_course_id: row[:course_id],
+      course_description: row[:course_descr],
+      title: row[:course_title_long],
+      term_name: term_name,
+      term_year: term_year,
+      academic_year: row[:academic_year],
+      class_section: row[:class_section].to_s =~ /^\d+$/ ? row[:class_section].to_i : row[:class_section],
+      component: row[:component_description],
+      subject: row[:subject],
+      subject_description: row[:subject_description],
+      subject_academic_org_description: row[:subject_acad_org_description],
+      class_academic_org_description: row[:class_acad_org_description],
+      academic_group: row[:acad_group],
+      academic_group_description: row[:acad_group_description],
+      grading_basis_description: row[:grading_gasis_description],
+      term_pattern_code: row[:term_pattern_code],
+      term_pattern_description: row[:term_pattern_description],
+      units_maximum: row[:units_maximum],
+      catalog_number: row[:catalog_number],
+      course_description_long: row[:course_descrlong],
+      course_note: row[:course_note]
+    )
+    puts "'#{c.course_description}' saved"
+  end
 end
--- db/seeds/division_mapping_seeds.rb
+++ db/seeds/division_mapping_seeds.rb
@@ -2,14 +2,15 @@ require 'smarter_csv'
 
 puts "Seeding DivisionMappings!"
 
-dm = SmarterCSV.process(Rails.root.join('lib', 'seeds', 'divisionMappings.csv'))
-dm.each do |row|
-  mapping = DivisionMapping.create(
-    academic_group: row[:academic_group],
-    subject_description: row[:subject_description],
-    division: row[:division],
-    division_description: row[:division_description]
-  )
+dm = SmarterCSV.process(Rails.root.join('lib', 'seeds', 'divisionMappings.csv'), chunk_size: 100)  do |chunk|
+  chunk.each do |row|
+    mapping = DivisionMapping.create(
+      academic_group: row[:academic_group],
+      subject_description: row[:subject_description],
+      division: row[:division],
+      division_description: row[:division_description]
+    )
 
-  puts "'#{mapping.division}' saved"
+    puts "'#{mapping.division}' saved"
+  end
 end
