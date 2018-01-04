32f7b93716ac03c5afcf16780c76fd6eaaea7fbb
Patrick Lewis <patrick@endpoint.com>
2018-01-04 14:26:09 +0000

    Lint user seed files

--- db/seeds/users_seeds.rb
+++ db/seeds/users_seeds.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 user = User.new(
-  {
-    :email => "testuser@endpoint.com",
-    :password => "testuser",
-    :password_confirmation => "testuser" 
-  }
+  email: 'testuser@endpoint.com',
+  password: 'testuser',
+  password_confirmation: 'testuser'
 ).save
+
 if user.nil?
-	puts "*** Added #{user.try(:email)}  ***"
+  puts "*** Added #{user.try(:email)}  ***"
 else
-	puts "*** User already exits ***"
+  puts '*** User already exists ***'
 end
--- lib/tasks/user.rake
+++ lib/tasks/user.rake
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 namespace :users do
-  desc "Seed users model)"
+  desc 'Seed users model)'
   task reseed: :environment do
     ActiveRecord::Base.transaction do
       require_relative '../../db/seeds/users_seeds'
