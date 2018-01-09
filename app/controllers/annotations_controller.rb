36d26c89e44f8d50bf3e620bff17342863a8cec6
Selvakumar Arumugam <selva@endpoint.com>
2018-01-09 12:47:36 +0000

    Add annotation model and api endpoints - 15043

---
+++ app/controllers/annotations_controller.rb
@@ -0,0 +1,24 @@
+class AnnotationsController < ApplicationController
+  before_action :authenticate_user!
+  #TODO Clean up after session and token implementation at frontend
+  skip_before_action :verify_authenticity_token
+
+  # add annotation to user's course in tray
+  def create
+    annotation_data = params["annotation"].strip
+    course = Course.find(params["course_id"])
+    new_annotation = Annotation.find_or_create_by(user_id: current_user.id, course_id: course.id)
+    new_annotation.annotation = annotation_data
+    new_annotation.save
+    render json: new_annotation
+  end
+
+  # find course's annotations for current user
+  def get_annotations
+    course = Course.find(params["course_id"])
+    annotations = Annotation.where(user_id: current_user.id, course_id: course.id)
+
+    render json: annotations
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,12 @@ Rails.application.routes.draw do
       delete :remove
     end
   end
+
+  resources :annotations do
+    collection do
+      get :get_annotations
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
