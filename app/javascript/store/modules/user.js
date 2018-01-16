ca54ec75a48a66f4d9c101d4fb1d881e123de376
David Christensen <david@endpoint.com>
2018-01-16 21:58:27 +0000

    Add some basic user_course-related state/action vuex modules

--- app/javascript/store/modules/user.js
+++ app/javascript/store/modules/user.js
@@ -1,19 +1,75 @@
 // Manipulation of user-related data
 
-const state = {
+import axios from 'axios'
+import _ from 'lodash'
 
+const state = {
+  courses: {},
+  filter: {},
 }
 
 const getters = {
+  coursesByDate (state) {
+    var filter = state.filter
+    var events = {}
 
+    if ((filter != undefined) && (Object.keys(filter).length > 0)) {
+      const semester = filter.value.split(" ")
+      _.forEach(state.courses.semester, (day, key) => {
+        events[key] = day.filter((item) => {
+          if (filter.name === 'term_name'){
+            return item.term_name ==  semester[0] && item.term_year == semester[1]
+          }
+          else{
+            return item[filter.name] == filter.value
+          }
+        })
+      })
+    }
+    else {
+      events = state.courses.semester
+    }
+    return events
+  },
+  coursesByYear (state) {
+    var filter = state.filter
+    var yearlyEvents
+
+    if ((filter != undefined) && (Object.keys(filter).length > 0)) {
+      yearlyEvents = {};
+      const semester = filter.value.split(" ")
+      _.forEach(courses.multi_year, (day, key) => {
+        yearlyEvents[key] = day.filter((item) => {
+          if (filter.name === 'term_name'){
+            return item.term_name ==  semester[0] && item.term_year == semester[1]
+          }
+          else {
+            return item[filter.name] == filter.value
+          }
+        })
+      })
+    }
+    else{
+      yearlyEvents = state.courses.multi_year
+    }
+  }
 }
 
 const actions = {
-
+  get_user_courses ({ commit }) {
+      const course_url = '/courses/user_courses'
+      axios
+      .get(course_url)
+      .then((response) => {
+        commit('user/SET_COURSES', response.data)
+      })
+  },
 }
 
 const mutations = {
-
+  SET_COURSES: (state, value) => {
+    state.courses = value
+  },
 }
 
 export default {
