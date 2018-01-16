53e6c0157b0927cade30c341657df2a7e9906d8b
Patrick Lewis <patrick@endpoint.com>
2018-01-16 21:05:10 +0000

    Add additional fields to GraphQL course type

--- app/graphql/types/course_type.rb
+++ app/graphql/types/course_type.rb
@@ -4,24 +4,15 @@ Types::CourseType = GraphQL::ObjectType.define do
   name 'Course'
   description 'Represents a course in Curricle'
 
-  field :academic_group, !types.String, 'Academic group'
-  field :catalog_number, !types.Int, 'Catalog number'
+  field :academic_group, types.String, 'Academic group'
+  field :academic_group_description, types.String, 'Academic group description'
+  field :academic_year, types.Int, 'Academic year'
+  field :catalog_number, types.Int, 'Catalog number'
+  field :class_academic_org_description, types.String, 'Class academic organization description'
+  field :class_section, types.String, 'Class section'
   field :component, types.String, 'Component'
-  field :course_description_long, types.String, 'Extended description'
-
-  field :course_meeting_patterns, types[Types::CourseMeetingPatternType], 'List of course meeting patterns' do
-    resolve(
-      lambda do |course, _args, _ctx|
-        BatchLoader.for(course.id).batch(default_value: []) do |course_ids, loader|
-          CourseMeetingPattern.where(course_id: course_ids).each do |course_meeting_pattern|
-            loader.call(course_meeting_pattern.course_id) { |memo| memo << course_meeting_pattern }
-          end
-        end
-      end
-    )
-  end
-
-  field :id, !types.ID, 'Unique ID'
+  field :course_description, types.String, 'Course description'
+  field :course_description_long, types.String, 'Extended course description'
 
   field :course_instructors, types[Types::CourseInstructorType], 'List of course instructors' do
     resolve(
@@ -35,9 +26,34 @@ Types::CourseType = GraphQL::ObjectType.define do
     )
   end
 
-  field :subject, !types.String, 'Subject'
-  field :term_name, !types.String, 'Term name'
-  field :term_year, !types.Int, 'Term year'
-  field :title, !types.String, 'Title'
+  field :course_meeting_patterns, types[Types::CourseMeetingPatternType], 'List of course meeting patterns' do
+    resolve(
+      lambda do |course, _args, _ctx|
+        BatchLoader.for(course.id).batch(default_value: []) do |course_ids, loader|
+          CourseMeetingPattern.where(course_id: course_ids).each do |course_meeting_pattern|
+            loader.call(course_meeting_pattern.course_id) { |memo| memo << course_meeting_pattern }
+          end
+        end
+      end
+    )
+  end
+
+  field :course_note, types.String, 'Course note'
+  field :created_at, !types.String, 'Created at'
+  field :division, types.String, 'Division'
+  field :division_description, types.String, 'Division description'
+  field :external_course_id, types.Int, 'External course ID'
+  field :grading_basis_description, types.String, 'Grading basis description'
+  field :id, !types.ID, 'Unique ID'
+  field :prereq, types.Int, 'Prerequisite'
+  field :subject, types.String, 'Subject'
+  field :subject_academic_org_description, types.String, 'Subject academic organization description'
+  field :subject_description, types.String, 'Subject description'
+  field :term_name, types.String, 'Term name'
+  field :term_pattern_code, types.String, 'Term pattern code'
+  field :term_pattern_description, types.String, 'Term pattern description'
+  field :term_year, types.Int, 'Term year'
+  field :title, types.String, 'Title'
   field :units_maximum, types.Int, 'Maximum units'
+  field :updated_at, !types.String, 'Updated at'
 end
