01b0134a03e9c5262d8e3ebf1c85536cfeacff2a
David Christensen <david@endpoint.com>
2018-01-16 21:58:13 +0000

    Add basic search vuex stuff

--- app/javascript/store/modules/search.js
+++ app/javascript/store/modules/search.js
@@ -1,11 +1,14 @@
 // Manipulation of search-related data
 
 const state = {
-
+  // list of objects with { text, weight, applyTo, active }
+  keywords: [],
+  results: []
 }
 
 const getters = {
-
+  activeKeywords: state => state.keywords.filter(kw => kw.active),
+  inactiveKeywords: state => state.keywords.filter(kw => !kw.active),
 }
 
 const actions = {
@@ -13,7 +16,7 @@ const actions = {
 }
 
 const mutations = {
-
+  
 }
 
 export default {
