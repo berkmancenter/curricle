92aa35a0d7f8f23cccabce28cb09593ca0e0a338
David Christensen <david@endpoint.com>
2018-01-17 00:40:28 +0000

    Whack unneeded(?) search changes

--- app/javascript/views/search.vue
+++ app/javascript/views/search.vue
@@ -4,7 +4,7 @@
       <div class="col-md-8">
         <basic-search @keywordsUpdated="keywords = $event"/>
         <br>
-        <curricle-search :keywords="keywords" :getResults="getResults" :getUserCourses="getUserCourses" :isBelongsToUser="isBelongsToUser" :isMeetingBelongsToUser="isMeetingBelongsToUser"/>
+        <curricle-search :keywords="keywords" />
       </div>
       <div class="col-md-4 sidebar" v-if="trayVisible">
         <tray/>
@@ -18,18 +18,12 @@ import { mapState } from 'vuex'
 import Tray from '../components/tray/tray.vue'
 import BasicSearch from '../components/BasicSearch.vue'
 import CurricleSearch from '../components/CurricleSearch.vue'
-import CourseList from '../components/tray/list.vue'
-import PlanFilter from '../components/plan/plan-filter.vue'
-import CalendarSidebar from 'components/plan/calendar-sidebar'
 import axios from 'axios'
 
 export default {
   components: {
     BasicSearch,
     CurricleSearch,
-    CourseList,
-    PlanFilter,
-    CalendarSidebar
     Tray,
   },
 
@@ -39,137 +33,21 @@ export default {
   props: [ 'searchedResults'],
 
   mounted () {
-    this.filterCategories()
-    this.getCoursesByDate()
-    this.getCoursesByYear()
-    this.getUserCourses()
+    //this.filterCategories()
+    //this.getCoursesByDate()
+    //this.getCoursesByYear()
+    //this.getUserCourses()
   },
 
   data () {
     return {
       keywords: [],
-      sideBarview: 'list-view',
-      userCourses: [],
       results: [],
-      filteredResults: [],
+      //filteredResults: [],
       categories: [],
-      events: [],
-      yearlyEvents: [],
-      currentFilter: {},
-      userCoursesIds: [],
-      userCoursesScheduleIds: []
     }
   },
 
-  methods: {
-    selectSideBarView(view){
-      this.sideBarview = view
-      this.filterData(this.currentFilter)
-    },
-
-    getResults(results){
-      this.results = results
-    },
-
-    getCoursesByDate(filter){
-      if((filter != undefined) && (Object.keys(filter).length > 0)){
-        this.events = {};
-        const semester = filter.value.split(" ")
-        _.forEach(this.userCourses.semester, (day, key) => {
-          this.events[key] = day.filter((item) => {
-            if (filter.name === 'term_name'){
-              return item.term_name ==  semester[0] && item.term_year == semester[1]
-            }
-            else{
-              return item[filter.name] == filter.value
-            }
-          })
-        })
-      }else{
-        this.events = this.userCourses.semester
-      }
-    },
-
-    getCoursesByYear(filter){
-      if((filter != undefined) && (Object.keys(filter).length > 0)){
-        this.yearlyEvents = {};
-        const semester = filter.value.split(" ")
-        _.forEach(this.userCourses.multi_year, (day, key) => {
-          this.yearlyEvents[key] = day.filter((item) => {
-            if (filter.name === 'term_name'){
-              return item.term_name ==  semester[0] && item.term_year == semester[1]
-            }
-            else{
-              return item[filter.name] == filter.value
-            }
-          })
-        })
-      }else{
-        this.yearlyEvents = this.userCourses.multi_year
-      }
-    },
-
-    filterCategories(){
-      axios.get('/courses/categories').then((response) => {
-        this.categories = response.data
-        .filter(item => item.name == 'Semester')
-      })
-    },
-
-    selectedFilter (filter, name) {
-      let data = this.results
-      this.currentFilter = filter
-
-      if (filter.value != 'none') {
-        this.filterData(filter)
-      }
-    },
-
-    filterInListView (filter) {
-      this.filteredResults = this.userCourses.tray
-      this.filteredResults = this.filteredResults.filter(item => {
-        if (filter.name === 'term_name'){
-          const semester = filter.value.split(" ")
-          return item.term_name ==  semester[0] && item.term_year == semester[1]
-        }
-        else{
-          return item[filter.name] == filter.value
-        }
-      })
-    },
-
-    filterData (filter) {
-      if(this.sideBarview == 'semester'){
-        this.getCoursesByDate(filter)
-      }
-
-      if(this.sideBarview == 'list-view'){
-        this.filterInListView(filter)
-      }
-
-      if(this.sideBarview == 'multi-year'){
-        this.getCoursesByYear(filter)
-      }
-    },
-    getUserCourses() {
-      const course_url = '/courses/user_courses'
-      axios
-        .get(course_url)
-        .then((response) => {
-          this.userCourses = response.data
-          this.filteredResults = this.userCourses.tray
-          this.getCoursesByDate(this.currentFilter)
-          this.userCoursesIds = this.userCourses.tray.map(item => { return item.id })
-          this.userCoursesScheduleIds = this.userCourses.tray.filter(item => !!item.user_schedule[0]).map(item => { return item.user_schedule[0].course_meeting_pattern_id })
-      })
-    },
-    isBelongsToUser(id){
-      return this.userCoursesIds.includes(id)
-    },
-    isMeetingBelongsToUser(id){
-      return this.userCoursesScheduleIds.includes(id)
-    }
-  }
 }
 </script>
 
