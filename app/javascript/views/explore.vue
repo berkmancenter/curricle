768aef686a39f2ffbe6d7286acd21aa17e514fd3
David Christensen <david@endpoint.com>
2018-01-22 23:10:24 +0000

    Add tray to Explore

--- app/javascript/views/explore.vue
+++ app/javascript/views/explore.vue
@@ -1,19 +1,49 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <hr>
+  <div>
+    <div class="row">
+      <div class="col-md-9">
+        <hr>
+        <b-jumbotron
+          header="Explore"
+          lead="View Template">
+          <p>Paragraph</p>
 
-      <b-jumbotron
-        header="Explore"
-        lead="View Template">
-        <p>Paragraph</p>
-
-        <b-btn
-          variant="primary"
-          href="#">
-          Button
-        </b-btn>
-      </b-jumbotron>
+          <b-btn
+            variant="primary"
+            href="#">
+            Button
+          </b-btn>
+        </b-jumbotron>
+      </div>
+      <div class="col-md-3 sidebar">
+        <tray v-if="trayVisible" />
+        <selected-course v-if="validCourseSelected" />
+      </div>
     </div>
   </div>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+import Tray from 'components/tray/tray'
+import SelectedCourse from 'components/plan/selected-course'
+
+export default {
+  components: {
+    SelectedCourse,
+    Tray
+  },
+
+  computed: {
+    ...mapState('app', ['trayVisible']),
+    ...mapGetters('user', ['validCourseSelected'])
+  }
+}
+</script>
+
+<style type="text/css">
+  .sidebar{
+    margin-top: 28px;
+  }
+</style>
