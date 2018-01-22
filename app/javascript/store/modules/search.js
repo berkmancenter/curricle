399791b6639c238de0c06accc0f23d01091a57d3
David Christensen <david@endpoint.com>
2018-01-22 18:02:23 +0000

    Add weight options into shared space

--- app/javascript/store/modules/search.js
+++ app/javascript/store/modules/search.js
@@ -17,6 +17,17 @@ const state = {
     { text: 'Instructor', value: 'Instructor' },
     { text: 'Readings', value: 'Readings', disabled: true },
     { text: 'Course ID', value: 'Course ID', disabled: true }
+  ],
+  weightOptions: [
+    { text: '1', value: 1 },
+    { text: '2', value: 2 },
+    { text: '3', value: 3 },
+    { text: '4', value: 4 },
+    { text: '5', value: 5 },
+    { text: '6', value: 6 },
+    { text: '7', value: 7 },
+    { text: '8', value: 8 },
+    { text: '9', value: 9 }
   ]
 }
 
