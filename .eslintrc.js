260bfee63cea2590de86b9e00056d07d95bbc640
Patrick Lewis <patrick@endpoint.com>
2018-01-24 14:30:28 +0000

    Disable no-debugger rule in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
   extends: [
     'standard',
     'plugin:vue/recommended'
-  ]
+  ],
+  rules: {
+    "no-debugger": 0 // TODO: Re-enable this rule after production launch
+  }
 }
