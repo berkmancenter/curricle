7238816c45bed8f3d3d37d68700e11f8cee77972
David Christensen <david@endpoint.com>
2018-01-29 20:10:09 +0000

    Create department-level selector using tray courses

---
+++ app/javascript/components/plan/department-selector.vue
@@ -0,0 +1,39 @@
+<template>
+  <b-dropdown
+    :text="department ? 'Department: ' + department : 'Department'"
+    class="m-md-2"
+  >
+    <b-dropdown-item @click="setDepartment(null)">
+      All Departments
+    </b-dropdown-item>
+    <b-dropdown-item
+      v-for="dept in departmentsInTray"
+      :key="dept"
+      @click="setDepartment(dept)"
+    >
+      {{ dept }}
+    </b-dropdown-item>
+  </b-dropdown>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  data () {
+    return {
+      department: ''
+    }
+  },
+  computed: {
+    ...mapGetters('plan', ['departmentsInTray'])
+  },
+  methods: {
+    ...mapActions('plan', ['setFilter']),
+    setDepartment (dept) {
+      this.department = dept
+      this.setFilter({name: 'department', value: dept})
+    }
+  }
+}
+</script>
