fdb4d0072d5d86433d30d1069cd027c20bde9d08
David Christensen <david@endpoint.com>
2018-01-29 17:36:03 +0000

    Remove relatively redundant list-item component; merge into list-view

--- app/javascript/components/plan/list-view.vue
+++ app/javascript/components/plan/list-view.vue
@@ -1,15 +1,67 @@
 <template>
   <div class="plan">
-    <plan-list-item/>
+    <table class="course-list-iltem">
+      <tbody>
+        <tr
+          v-for="course in trayCourses"
+          :key="course.id"
+          @click="selectCourse(course)"
+        >
+          <td>{{ course.external_course_id }}</td>
+          <td style = "width: 12em;">{{ course.course_description }}</td>
+          <td>{{ course.academic_group }} <br> {{ course.subject }}</td>
+          <td>{{ course.term_name }} <br> {{ course.units_maximum }} units</td>
+          <td style = "width: 16em; text-align: justify;">
+            <truncate
+              class="course_description"
+              clamp="..."
+              :length="50"
+              less="Show Less"
+              type="html"
+              :text="course.course_description_long"
+              v-if="course.course_description_long"/>
+          </td>
+          <td>
+            <class-meeting-time
+              :days="course.days"
+              :condensed="true"
+            />
+          </td>
+          <td style="border-right: 5px solid #000; position: relative; font-size: 24px;">
+            <course-action
+              type="schedule"
+              :course="course.id"
+            />
+          </td>
+          <span class= "check_box">
+            <input
+              type="checkbox"
+              name=""
+              value="">
+          </span>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import PlanListItem from 'components/plan/list-item'
+import { mapGetters, mapActions } from 'vuex'
+import truncate from 'vue-truncate-collapsed'
+import CourseAction from 'components/shared/course-action'
+import ClassMeetingTime from 'components/shared/ClassMeetingTime'
 
 export default {
   components: {
-    PlanListItem
+    truncate,
+    CourseAction,
+    ClassMeetingTime
+  },
+  computed: {
+    ...mapGetters('user', ['trayCourses'])
+  },
+  methods: {
+    ...mapActions('user', ['selectCourse'])
   }
 }
 </script>
@@ -39,4 +91,44 @@ export default {
     border-width: 2px !important;
     margin-top: 0px !important;
   }
+
+  .course-list-iltem tbody tr{
+    background: #DCDCDC;
+    font-size: 12px;
+    cursor: pointer;
+  }
+  .course-list-iltem tbody tr:hover {
+    background: #C0C0C0;
+  }
+  .course-list-iltem tbody tr {
+    margin-bottom: 5px;
+    width: 100%;
+    vertical-align: middle;
+  }
+  .course-list-iltem thead {
+    display: table-caption;
+    width: 100%;
+  }
+  .course-list-iltem tbody tr .check_box {
+    display: inline-block;
+    margin-top: 14px;
+    position: absolute;
+    right: 0px;
+  }
+  .course-list-iltem tbody {
+    border-top: 2px solid #C0C0C0;
+  }
+  .table td {
+    vertical-align: middle !important;
+  }
+  .user-schedule{
+    color: gray;
+  }
+  .course_description p {
+    padding-top: 13px !important;
+  }
+  .plan table td{
+    padding: 0px 10px;
+  }
+
 </style>
