a88b787a7f444c2d4a33896d567acc4d9183aacc
David Christensen <david@endpoint.com>
2018-01-30 19:34:40 +0000

    Make multi-year views editable too

--- app/javascript/components/tray/multi-year-sidebar.vue
+++ app/javascript/components/tray/multi-year-sidebar.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="calendar-sidebar">
+  <div class="multi-year-sidebar">
     <div
-      v-for="semester in sortedSemestersInSchedule.slice().reverse()"
+      v-for="semester in sortedSemestersInTray.slice().reverse()"
       :key="semester">
       <strong>{{ semester }}</strong>
+      <div
+        v-text="isEditing[semester] ? 'Done' : 'Edit'"
+        @click="toggleEditSemester(semester)"
+        class="pull-right"
+      />
       <course-list
-        :courses="scheduledCoursesBySemester[semester] || []"
+        :courses="(isEditing[semester] ? trayCoursesBySemester[semester] : scheduledCoursesBySemester[semester]) || []"
+        :editable="isEditing[semester]"
       />
     </div>
   </div>
@@ -14,14 +20,28 @@
 <script>
 import { mapState, mapGetters } from 'vuex'
 import CourseList from 'components/shared/course-list'
+import _ from 'lodash'
 
 export default {
   components: {
     CourseList
   },
+  data () {
+    return {
+      isEditing: {}
+    }
+  },
   computed: {
     ...mapState('app', ['viewmode']),
-    ...mapGetters('plan', ['scheduledCoursesBySemester', 'sortedSemestersInSchedule'])
+    ...mapGetters('plan', ['scheduledCoursesBySemester', 'trayCoursesBySemester', 'sortedSemestersInTray'])
+  },
+  mounted () {
+    _.each(this.sortedSemestersInTray, d => { this.$set(this.isEditing, d, false) })
+  },
+  methods: {
+    toggleEditSemester (sem) {
+      this.isEditing[sem] = !this.isEditing[sem]
+    }
   }
 }
 </script>
