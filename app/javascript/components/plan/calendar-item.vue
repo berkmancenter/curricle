2d1873e522a9771cf8df3717cef849bba2202d08
David Christensen <david@endpoint.com>
2018-02-01 23:29:12 +0000

    Fixes for calendar items which collapsed instead of expanded on hover

--- app/javascript/components/plan/calendar-item.vue
+++ app/javascript/components/plan/calendar-item.vue
@@ -3,17 +3,15 @@
     :class="{'calendar-item': true, provisional }"
     :style="computedStyle"
     @click="selectCourse(item.id)">
-    <span :style="{ height: scale * height }">
-      <strong>{{ item.external_course_id }}</strong>
+    <strong>{{ item.external_course_id }}</strong>
+    <br>
+    {{ item.title }}
+    <br>
+    <strong>
+      {{ item.academic_group }}
       <br>
-      {{ item.title }}
-      <br>
-      <strong>
-        {{ item.academic_group }}
-        <br>
-        {{ item.subject }} {{ item.catalog_number }}
-      </strong>
-    </span>
+      {{ item.subject }} {{ item.catalog_number }}
+    </strong>
   </div>
 </template>
 
@@ -51,11 +49,15 @@ export default {
     computedStyle () {
       var top = this.scale * this.offset + 25 + this.nudge
       return {
-        height: this.scale * this.height + 'px',
         top: top + 'px',
         'border-top': '10px solid ' + (this.item.department_color || '#000')
       }
     }
+  },
+  mounted () {
+    // this is fine for now since we're not changing property values
+    var style = this.$el.style
+    style.setProperty('--height', (this.scale * this.height) + 'px')
   },
   methods: {
     ...mapActions('user', ['selectCourse'])
@@ -73,6 +75,8 @@ export default {
   padding-right: .5em;
   font-size: 12px;
   width: 90%;
+  height: var(--height);
+  min-height: var(--height);
   border-radius: 5px;
   border: 1px solid rgba(0,0,0,0.3);
   background-clip: padding-box;
@@ -82,7 +86,7 @@ export default {
   background-color: #C0C0C0;
   z-index: 10;
   box-shadow: 0 0 15px black;
-  height: auto !important;
+  height: auto;
 }
 
 .calendar-item.provisional {
