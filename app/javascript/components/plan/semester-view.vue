e8487da5a2cc53586b298873a24e5cb7d50c432c
David Christensen <david@endpoint.com>
2018-02-02 16:12:24 +0000

    Fix off-by-one error in the semester calendar time

--- app/javascript/components/plan/semester-view.vue
+++ app/javascript/components/plan/semester-view.vue
@@ -136,7 +136,7 @@ export default {
       ))
     },
     times () {
-      return ['1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm',
+      return ['midnight', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm',
         '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12am' ]
         .slice(this.earliestIdx, this.latestIdx)
     },
