7caf3c3dcd8ba79f5cab858590fe0154000b6939
David Christensen <david@endpoint.com>
2018-02-02 16:05:12 +0000

    Make overlapping items more distinct

--- app/javascript/components/plan/calendar-item.vue
+++ app/javascript/components/plan/calendar-item.vue
@@ -84,6 +84,7 @@ export default {
   border-radius: 5px;
   border: 1px solid rgba(0,0,0,0.3);
   background-clip: padding-box;
+  box-shadow: 0 0 10px rgba(32,32,32,0.5);
 }
 
 .calendar-item:hover {
