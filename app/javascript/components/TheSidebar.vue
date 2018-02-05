31d6b382b59263c7cf71d93bfbe8243acd02408e
David Christensen <david@endpoint.com>
2018-02-05 16:53:30 +0000

    New TheSidebar component to support any/all sidebar-like views

---
+++ app/javascript/components/TheSidebar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <tray
+      v-if="sidebarCurrentType==='tray'"
+    />
+    <selected-course
+      v-if="sidebarCurrentType==='course'"
+      :course="sidebarCurrentContext"
+    />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Tray from 'components/tray/tray'
+import SelectedCourse from 'components/shared/SelectedCourse'
+
+export default {
+  components: {
+    Tray,
+    SelectedCourse
+  },
+  computed: {
+    ...mapGetters('app', ['sidebarCurrentType', 'sidebarCurrentContext'])
+  }
+}
+</script>
