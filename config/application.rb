50dc4197da5b3128709ab91ee0e6f022c645dab4
David Christensen <david@endpoint.com>
2018-02-08 18:34:46 +0000

    Fix timezone for proper course times
    This may cause issues in other places, but on the other hand it may
    not. :-)

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Curricle
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.time_zone = 'America/New_York'
+    config.time_zone = 'UTC'
 
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir.glob("#{config.root}/app/interactions/*")
