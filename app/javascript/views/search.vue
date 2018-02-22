3054d029a1903a71a2c0be8f02e04840fa06311a
Patrick Lewis <patrick@patricklewis.org>
2018-02-22 16:28:01 -0500

    Hide search form from anonymous users

--- app/javascript/views/search.vue
+++ app/javascript/views/search.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row mx-0">
     <div class="col-md-9 d-flex flex-column">
-      <search-form />
+      <search-form v-if="userAuthenticated"/>
       <br>
       <search-results />
     </div>
@@ -12,6 +12,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import SearchForm from 'components/search/SearchForm'
 import SearchResults from 'components/search/SearchResults'
 import TheSidebar from 'components/TheSidebar'
@@ -21,6 +22,9 @@ export default {
     SearchForm,
     SearchResults,
     TheSidebar
+  },
+  computed: {
+    ...mapGetters('user', ['userAuthenticated'])
   }
 }
 </script>
