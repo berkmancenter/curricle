cb3f019b3e5253306c85959f0c03d4fe779c5b2d
Patrick Lewis <patrick@patricklewis.org>
2018-05-09 09:26:10 -0400

    Add new Explore page layout
    Resolves: #15340

--- app/javascript/views/explore.vue
+++ app/javascript/views/explore.vue
@@ -1,19 +1,82 @@
 <template>
   <div class="row mx-0">
     <div class="col-md-9">
-      <hr>
-      <b-jumbotron
-        header="Explore"
-        lead="View Template">
-        <p>Paragraph</p>
+      <h1 class="my-3">Explore</h1>
 
-        <b-btn
-          variant="primary"
-          href="#">
-          Button
-        </b-btn>
-      </b-jumbotron>
+      <b-card-group
+        deck
+        class="mb-5">
+        <b-card title="Lecture Networks">
+          <p class="card-text">Show connections between course instructors</p>
+          <b-button
+            href="#"
+            variant="primary">
+            Explore lecture networks
+          </b-button>
+        </b-card>
+        <b-card title="Bar Comparison">
+          <p class="card-text">Investigate class types and departments</p>
+          <b-button
+            href="#"
+            variant="primary">
+            Explore bar comparison
+          </b-button>
+        </b-card>
+      </b-card-group>
+
+      <h5 class="mb-3">Other Exploratory Micronarrative Links:</h5>
+
+      <b-card-group columns>
+        <b-card
+          class="text-center"
+          header="Header">
+          <b-button href="#">
+            Go somewhere
+          </b-button>
+        </b-card>
+
+        <b-card
+          class="text-center"
+          header="Header">
+          <b-button href="#">
+            Go somewhere
+          </b-button>
+        </b-card>
+
+        <b-card
+          class="text-center"
+          header="Header">
+          <b-button href="#">
+            Go somewhere
+          </b-button>
+        </b-card>
+
+        <b-card
+          class="text-center"
+          header="Header">
+          <b-button href="#">
+            Go somewhere
+          </b-button>
+        </b-card>
+
+        <b-card
+          class="text-center"
+          header="Header">
+          <b-button href="#">
+            Go somewhere
+          </b-button>
+        </b-card>
+
+        <b-card
+          class="text-center"
+          header="Header">
+          <b-button href="#">
+            Go somewhere
+          </b-button>
+        </b-card>
+      </b-card-group>
     </div>
+
     <div class="col-md-3 top-header">
       <the-sidebar />
     </div>
