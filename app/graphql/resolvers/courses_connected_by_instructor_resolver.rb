270c1913d4ffeb7257f7b0c51658d26ff076b9ae
Patrick Lewis <patrick@patricklewis.org>
2018-05-15 10:32:46 -0400

    Improve performance of CoursesConnectedByInstructorResolver
    Scope results to only look at courses for the past 2-3 term years.

--- app/graphql/resolvers/courses_connected_by_instructor_resolver.rb
+++ app/graphql/resolvers/courses_connected_by_instructor_resolver.rb
@@ -4,11 +4,18 @@ module Resolvers
   # Return a collection of courses taught by instructors connected to a given instructor
   class CoursesConnectedByInstructorResolver
     def call(_obj, args, _ctx)
-      course_ids_taught_by_instructor = CourseInstructor.where(email: args[:email]).pluck(:course_id)
+      term_year_limit = 2.years.ago.year
+
+      course_ids_taught_by_instructor =
+        CourseInstructor
+        .where(email: args[:email])
+        .where("term_year >= #{term_year_limit}")
+        .pluck(:course_id)
 
       instructor_emails =
         CourseInstructor
         .where(course_id: course_ids_taught_by_instructor)
+        .where("term_year >= #{term_year_limit}")
         .where.not(email: args[:email])
         .distinct
         .pluck(:email)
@@ -16,6 +23,7 @@ module Resolvers
       course_ids_taught_by_connected_instructors =
         CourseInstructor
         .where(email: instructor_emails)
+        .where("term_year >= #{term_year_limit}")
         .distinct
         .pluck(:course_id)
 
