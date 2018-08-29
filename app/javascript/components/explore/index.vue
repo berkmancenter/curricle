99ad1bcde34db1fb619b495f8b3b068c266ba243
Alexandra DM <alexandra.dolanmescal@gmail.com>
2018-08-28 20:13:52 -0400

    reintroduce buttons
    router link created layout issue I don't have time to resolve right now, so they have been removed and replaced with the old button links.

--- app/javascript/components/explore/index.vue
+++ app/javascript/components/explore/index.vue
@@ -3,9 +3,6 @@
     <b-card-group
       deck
       class="mb-5">
-      <router-link
-        to="/explore/classes"
-        variant="primary">
       <b-card
         img-src="/images/explore/departments.png"
         img-top
@@ -13,11 +10,14 @@
         <p class="card-text">
           Explore courses by selecting class learning modes and departments.
         </p>
+        <div slot="footer">
+          <b-button
+            to="/explore/classes"
+            variant="primary">
+            View
+          </b-button>
+        </div>
       </b-card>
-      </router-link>
-      <router-link
-        to="/explore/instructor"
-        variant="primary">
       <b-card
         img-src="/images/explore/instructors.png"
         img-top
@@ -25,11 +25,14 @@
         <p class="card-text">
           Select an instructor to discover faculty networks by course and department.
         </p>
+        <div slot="footer">
+          <b-button
+            to="/explore/instructor"
+            variant="primary">
+            View
+          </b-button>
+        </div>
       </b-card>
-      </router-link>
-      <router-link
-        to="/explore/keywords"
-        variant="primary">
       <b-card
         img-src="/images/explore/keywords.png"
         img-top
@@ -37,11 +40,14 @@
         <p class="card-text">
           Visualize descriptive terms across the curriculum through keyword comparisons.
         </p>
+        <div slot="footer">
+          <b-button
+            to="/explore/keywords"
+            variant="primary">
+            View
+          </b-button>
+        </div>
       </b-card>
-      </router-link>
-      <router-link
-        to="/explore/universe"
-        variant="primary">
       <b-card
         img-src="/images/explore/courses.png"
         img-top
@@ -49,8 +55,14 @@
         <p class="card-text">
           Explore all of Harvard's departments and programs to browse their offerings.
         </p>
+        <div slot="footer">
+          <b-button
+            to="/explore/universe"
+            variant="primary">
+            View
+          </b-button>
+        </div>
       </b-card>
-      </router-link>
     </b-card-group>
   </div>
 </template>
