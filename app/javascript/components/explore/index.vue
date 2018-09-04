0be17c46787c650cd3535bdcd12787832bcbb084
Patrick Lewis <patrick@endpoint.com>
2018-09-04 19:41:11 +0000

    Fix layout of Explore index page

--- app/javascript/components/explore/index.vue
+++ app/javascript/components/explore/index.vue
@@ -3,54 +3,74 @@
     <b-card-group
       deck
       class="mb-5">
-      <router-link
-        to="/explore/classes"
-        variant="primary">
-        <b-card
-          img-src="/images/explore/departments.png"
-          img-top
-          title="Learning Modes">
-          <p class="card-text">
-            Explore courses by selecting class learning modes and departments.
-          </p>
-        </b-card>
-      </router-link>
-      <router-link
-        to="/explore/instructor"
-        variant="primary">
-        <b-card
-          img-src="/images/explore/instructors.png"
-          img-top
-          title="Instructor Networks">
-          <p class="card-text">
-            Select an instructor to discover faculty networks by course and department.
-          </p>
-        </b-card>
-      </router-link>
-      <router-link
-        to="/explore/keywords"
-        variant="primary">
-        <b-card
-          img-src="/images/explore/keywords.png"
-          img-top
-          title="Truth vs. Lies">
-          <p class="card-text">
-            Visualize descriptive terms across the curriculum through keyword comparisons.
-          </p>
-        </b-card>
-      </router-link>
-      <router-link
-        to="/explore/universe"
-        variant="primary">
-        <b-card
-          img-src="/images/explore/courses.png"
-          img-top
-          title="Course Universe">
-          <p class="card-text">
-            Explore all of Harvard's departments and programs to browse their offerings.
-          </p>
-        </b-card>
-      </router-link>
+      <b-card
+        img-src="/images/explore/departments.png"
+        img-top
+        title="Learning Modes">
+        <p class="card-text">
+          Explore courses by selecting class learning modes and departments.
+        </p>
+
+        <div slot="footer">
+          <b-button
+            to="/explore/classes"
+            variant="primary">
+            View
+          </b-button>
+        </div>
+      </b-card>
+
+      <b-card
+        img-src="/images/explore/instructors.png"
+        img-top
+        title="Instructor Networks">
+        <p class="card-text">
+          Select an instructor to discover faculty networks by course and department.
+        </p>
+
+        <div slot="footer">
+          <b-button
+            to="/explore/instructor"
+            variant="primary">
+            View
+          </b-button>
+        </div>
+      </b-card>
+
+      <b-card
+        img-src="/images/explore/courses.png"
+        img-top
+        title="Course Universe">
+        <p class="card-text">
+          Explore all of Harvard's departments and programs to browse their offerings.
+        </p>
+
+        <div slot="footer">
+          <b-button
+            to="/explore/universe"
+            variant="primary">
+            View
+          </b-button>
+        </div>
+      </b-card>
+
+      <b-card
+        img-src="/images/explore/keywords.png"
+        img-top
+        title="Truth vs. Lies">
+        <p class="card-text">
+          Visualize descriptive terms across the curriculum through keyword comparisons.
+        </p>
+
+        <div slot="footer">
+          <b-button
+            to="/explore/keywords"
+            variant="primary">
+            View
+          </b-button>
+        </div>
+      </b-card>
+
     </b-card-group>
   </div>
 </template>
