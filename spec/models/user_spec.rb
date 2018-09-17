f71ca4ee0f1cb443ca44f85e85579016a526219b
Patrick Lewis <patrick@patricklewis.org>
2018-09-17 10:52:12 -0400

    Add test coverage for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:annotations) }
+  it { is_expected.to have_many(:tags) }
+  it { is_expected.to have_many(:user_courses) }
+  it { is_expected.to have_many(:courses).through(:user_courses) }
 end
