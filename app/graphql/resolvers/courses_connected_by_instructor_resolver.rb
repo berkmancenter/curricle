fc282073654acb0eef76270a7344061c0d668b11
Patrick Lewis <patrick@patricklewis.org>
2018-09-25 22:06:23 -0400

    Expand instructor connections to past decade

--- app/graphql/resolvers/courses_connected_by_instructor_resolver.rb
+++ app/graphql/resolvers/courses_connected_by_instructor_resolver.rb
@@ -25,7 +25,7 @@ module Resolvers
 
     def determine_term_year_range(semester)
       term_year_max = semester[:term_year]
-      term_year_min = term_year_max - 2
+      term_year_min = term_year_max - 10
 
       term_year_min..term_year_max
     end
