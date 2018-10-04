9a61f51f74d30496db80ebd25f51103d97824ed4
Patrick Lewis <patrick@endpoint.com>
2018-10-04 19:28:52 +0000

    Update production CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -21,7 +21,7 @@ Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval, :unsafe_inline
   else
-    policy.script_src :self, :https
+    policy.script_src :self, :https, :unsafe_inline
   end
 end
 
