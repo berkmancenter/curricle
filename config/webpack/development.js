8cd8a2d317d9e95c523070456fa271d8155cf275
Patrick Lewis <patrick@patricklewis.org>
2018-10-18 11:45:59 -0400

    Restore eslint-loader in webpack configuration

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,8 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const eslintConfig = require('./development/eslint')
+
+environment.config.merge(eslintConfig)
 
 module.exports = environment.toWebpackConfig()
