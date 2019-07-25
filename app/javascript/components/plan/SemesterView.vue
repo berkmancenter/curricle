292a44277d6e703739f2ba35faffe4d64bae03f9
Patrick Lewis <patrick@patricklewis.org>
2019-07-25 10:12:01 -0400

    Update SemesterView
    Resolves: #16805

--- app/javascript/components/plan/SemesterView.vue
+++ app/javascript/components/plan/SemesterView.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <h3>
-      Plan:
+      Plan by Semester:
     </h3>
+
+    <my-harvard-link
+      v-if="scheduledCourses"
+      :courses="scheduledCourses"
+      class="my-4"
+    />
 
     <semester-credit-hours
       v-for="semester in sortedSemestersInSchedule"
@@ -15,14 +21,16 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import MyHarvardLink from './shared/MyHarvardLink'
 import SemesterCreditHours from './SemesterViewSemesterCreditHours'
 
 export default {
   components: {
+    MyHarvardLink,
     SemesterCreditHours
   },
   computed: {
-    ...mapGetters('plan', ['sortedSemestersInSchedule', 'scheduledCoursesBySemester'])
+    ...mapGetters('plan', ['sortedSemestersInSchedule', 'scheduledCoursesBySemester', 'scheduledCourses'])
   }
 }
 </script>
